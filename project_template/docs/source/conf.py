20363dc4b6ffea31c612ef7c9d44b4fcc6def7eb
Piotr Plucinski <plutocamera@gmail.com>
2025-01-11 15:56:59 +0100

    sphinx doc with config

--- project_template/docs/source/conf.py
+++ project_template/docs/source/conf.py
@@ -1,3 +1,7 @@
+import sys, os
+
+sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,15 +18,13 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
---
+++ project_template/project_template/funk.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    """Sum of two numbers
+
+    :param a: int one
+    :param b: int two
+    :return: int return
+    """
+    return a+b
