f5c4d42956c753ae80c9d436012092ddf9830c78
Piotr Plucinski <plutocamera@gmail.com>
2025-01-11 22:10:08 +0100

    ruff formater config

--- project_template/project_template/funk.py
+++ project_template/project_template/funk.py
@@ -1,8 +1,14 @@
-def add(a,b):
+def add(a, b):
     """Sum of two numbers
 
     :param a: int one
     :param b: int two
     :return: int return
     """
-    return a+b
+    return a + b
+
+
+x = 32
+
+def yolo():
+    return x
